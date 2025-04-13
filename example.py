33499ebb94dca51f4b18667d72669773c2c63314
Andrei <drop.all.requests@gmail.com>
2025-04-13 23:41:05 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,17 +1,17 @@
 import asyncio
-
 from src.image_sitemap import Sitemap
 from src.image_sitemap.instruments.config import Config
 
 config = Config(
     max_depth=1,
-    accept_subdomains=True,
+    accept_subdomains=False,
     is_query_enabled=False,
-    file_name="example_sitemap_images.xml",
+    file_name="sitemap.xml",
     header={
         "User-Agent": "ImageSitemap Crawler",
         "Accept": "text/html",
     },
 )
 
-asyncio.run(Sitemap(config=config).run(url="https://rucaptcha.com/"))
+asyncio.run(Sitemap(config=config).run_images_sitemap(url="https://rucaptcha.com/"))
+asyncio.run(Sitemap(config=config).run_sitemap(url="https://rucaptcha.com/"))
